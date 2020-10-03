e6bc34f1b28e6e6d8d0153890c7d38fefbdebed0
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-10-03 14:15:15 -0700

    class definition

--- test3.py
+++ test3.py
@@ -1,1 +1,15 @@
 class user:
+    firstName = "Mahrokh"
+    lastName = "Ebrahimi"
+    age = 34
+    def showFullName(self):
+    pass
+
+#print(type('python'))   # <class 'str'>
+#numbers = list()
+
+nemune = user()
+
+print(nemune.firstName)
+print(nemune.lastName)
+print(nemune.age)
