ec7c2270178db22b4c7e93b38372f8142a6c4621
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-10-04 20:58:39 -0700

    class definition

--- test3.py
+++ test3.py
@@ -3,13 +3,20 @@ class user:
     lastName = "Ebrahimi"
     age = 34
     def showFullName(self):
-    pass
-
+        pass
 #print(type('python'))   # <class 'str'>
 #numbers = list()
-
 nemune = user()
-
 print(nemune.firstName)
 print(nemune.lastName)
 print(nemune.age)
+
+# making a new class:
+class car():
+    name = 'BMW'
+    model = 2020
+    manufacture = 'karkhune bmw sazi'
+    def carIn(self):
+        pass
+carNemune = car()
+print(carNemune.name)
