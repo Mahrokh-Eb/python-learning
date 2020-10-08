aeafd8963b15e5eb52c47aa9d941d51784c85261
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-10-07 19:08:08 -0700

    class definition part two

---
+++ test4.py
@@ -0,0 +1,13 @@
+# defining class
+class books():
+    spirtualBooks = 'bible'
+    poembooks = 'Rumi'
+    scientificBooks = 'statistic'
+    def bbookkss(self):
+        return 'it is function'
+    def __init__(self):
+
+
+sampleBook = books()
+print(sampleBook.spirtualBooks)
+print(sampleBook.bbookkss())
