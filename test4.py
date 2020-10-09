23cc77801bc04e915e994838f650cdb88aa80ec3
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-10-08 19:00:20 -0700

    class definition part two

--- test4.py
+++ test4.py
@@ -1,13 +1,17 @@
-# defining class
-class books():
+# defining class- 59
+class books:
     spirtualBooks = 'bible'
     poembooks = 'Rumi'
     scientificBooks = 'statistic'
     def bbookkss(self):
-        return 'it is function'
-    def __init__(self):
+        x = self.spirtualBooks +' and '+ self.poembooks
+        return x
+
+    #def __init__(self):
 
 
 sampleBook = books()
 print(sampleBook.spirtualBooks)
 print(sampleBook.bbookkss())
+print(type(sampleBook))  #<class '__main__.books'>
+print(__name__)   #__main__ , neme=sampleBook
