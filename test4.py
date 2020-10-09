88be301a9e87c94446e85b9de102d97a25d8febc
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-10-09 16:33:01 -0700

    class definition part three

--- test4.py
+++ test4.py
@@ -1,17 +1,22 @@
 # defining class- 59
 class books:
+    # attribute in class
     spirtualBooks = 'bible'
     poembooks = 'Rumi'
     scientificBooks = 'statistic'
+    # action in class
     def bbookkss(self):
         x = self.spirtualBooks +' and '+ self.poembooks
         return x
 
-    #def __init__(self):
+    def __init__(self, sbook, pbook):
+        self.spirtualBooks = sbook
+        self.poembooks = pbook
 
 
-sampleBook = books()
-print(sampleBook.spirtualBooks)
-print(sampleBook.bbookkss())
-print(type(sampleBook))  #<class '__main__.books'>
-print(__name__)   #__main__ , neme=sampleBook
+sampleBook = books('havim', 'colli')
+#print(sampleBook.spirtualBooks)
+#print(sampleBook.bbookkss())
+#print(type(sampleBook))  #<class '__main__.books'>
+#print(__name__)   #__main__ , neme=sampleBook
+print(sampleBook.bbookkss( ))
