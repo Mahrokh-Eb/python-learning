12669baba28d26a4b178e6e3f05693e0784b0e71
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-10-10 13:49:59 -0700

    class definition part 6 importing

---
+++ test7.py
@@ -0,0 +1,9 @@
+# lecture 60
+from test5 import car
+class user():
+    userName = 'mahi'
+    def __init__(self):
+        PDC.reconsideration @ sba.gov
+
+perid = car('perid', 2, 'redveie')
+print(perid.showCarInfo())
