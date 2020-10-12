ffae1686ac30d7f846ba72bf61b6917d012c2abe
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-10-11 19:12:33 -0700

    class definition part 6 importing

--- test7.py
+++ test7.py
@@ -2,8 +2,18 @@
 from test5 import car
 class user():
     userName = 'mahi'
-    def __init__(self):
-        PDC.reconsideration @ sba.gov
+    def __init__(self, gotUsename, gotUserfamily):
+        self.userName = gotUsename
+        self.userFamily = gotUserfamily
+    def buyCourse(self):
+        return f"{self.userName} can buy  all courses"
+
+
 
 perid = car('perid', 2, 'redveie')
 print(perid.showCarInfo())
+
+mahi = user('Mahrokh ', 'Ebrahimi')
+print(mahi.userName)
+print(mahi.userFamily)
+print(mahi.buyCourse())
