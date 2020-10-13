3b0d2e91de71662c8dea149b6f6bba1ff28869b2
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-10-12 19:25:25 -0700

    class definition part 7

--- test8.py
+++ test8.py
@@ -2,9 +2,12 @@
 
 class user():
     def __init__(self, userName):
-        self.userName = userName
+        self.userName = "userName"
+        self._password = "userPass"
         pass
     def __sayhello():
         pass
 
 me = user('golbanoo')
+print(me.userName)
+print(me._password)
