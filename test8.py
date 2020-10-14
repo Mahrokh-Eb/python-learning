d9054e3dd3cfca01e60b3f2d89827087f2ea688d
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-10-13 21:59:06 -0700

    class definition part 7

--- test8.py
+++ test8.py
@@ -3,7 +3,8 @@
 class user():
     def __init__(self, userName):
         self.userName = "userName"
-        self._password = "userPass"
+        self._password = "userPass" # it is private, if you hit dot after me, it won't show up
+
         pass
     def __sayhello():
         pass
@@ -11,3 +12,4 @@ class user():
 me = user('golbanoo')
 print(me.userName)
 print(me._password)
+print('done!')
