0325eeda3aaed6a3279ade0b62574c29cca756e2
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-10-19 12:57:19 -0700

    class definition part 9

--- test10.py
+++ test10.py
@@ -4,7 +4,15 @@ class user:
     def __init__(self, userName):
         self.userName = userName
         self._email = 'test@test.com'
+        self._passWord = 'pass'
+
+    def login(self, gotPassword):
+        if self._passWord == gotPassword:
+            print('let him in')
+        else:
+            print('wrong password')
 
 insta = user('mahi')
 print(insta.userName)
 print(insta._email)
+insta.login("pass")
