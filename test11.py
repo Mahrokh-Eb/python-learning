5a32cc3d0816a82d337a2948669e7f8271ca8d08
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-10-20 18:12:49 -0700

    class definition part 10

--- test11.py
+++ test11.py
@@ -3,9 +3,14 @@ class user():
     def __init__(self, gotName, gotFamily, gotAge):
         self.name = gotName
         self.family = gotFamily
-        self.family = gotFamily
+        self.age = gotAge
     def showFullName(self ):
         return f"{self.name} {self.family} {self.age}"
+    def userAgeStatus(self):
+        return f'{self.name} " "' if self.age>40 else f'{self.name} "not adult"'
 
 me = user('mahi', 'golkhand', 32)
-
+print(me.showFullName())
+she = user('sara', 'golbahani', 82)
+print(she.showFullName())
+print(me.userAgeStatus())
