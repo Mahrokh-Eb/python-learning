8906609fab0fe417238e8b88ab1640d291aa775c
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-10-30 19:53:57 -0700

    lecture 63

---
+++ test16.py
@@ -0,0 +1,22 @@
+#lecture 63
+class Person:
+  users = ['mohammad', 'milad']
+
+  def __init__(self, name):
+    self.name = name
+
+
+me = Person('mohammad')
+
+# print(Person.users)
+# Person.users = ['sara']
+# print(me.users)
+
+
+print(Person.users)
+
+print(me.users)
+
+me.users = ['sara']
+
+print(Person.users)
